684e2b8d8b278fd764fe9cf4716edbbfa078a017
rainr <rain.rahni@mail.ee>
2024-06-01 12:52:24 +0300

    add initial files and sidebar complete

---
+++ cloudchat/src/routes/+page.svelte
@@ -0,0 +1,171 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sidebar Component</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css" rel="stylesheet">
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <style>
+        body {
+            background-color: #f8f9fa;
+            font-family: 'Poppins';
+            color: #1B2559;
+        }
+        .sidebar {
+            height: 92vh;
+            width: 300px;
+            background-color: #ffffff;
+            border-radius: 20px;
+            border-right: 1px solid #e3e3e3;
+            padding: 20px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+            display: flex;
+            flex-direction: column;
+        }
+        .sidebar .logo {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            font-size: 1.5rem;
+            font-weight: normal;
+            padding: 30px;
+            margin-bottom: 30px;
+            border-bottom: 1px solid #e3e3e3;
+        }
+        .sidebar .nav-heading {
+            display: flex;
+            font-weight: bold;
+            padding-bottom: 10px;
+            margin: 10px 10px;
+        }
+        .sidebar .user-info {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: auto;
+            padding: 10px;
+            font-weight: bold;
+            border-radius: 30px;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+        .logout-button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            border: 1px solid #E0E5F2;
+            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        }
+        .logout-button svg {
+            width: 70%;
+            height: 70%;
+        }
+        .sidebar .user-info img {
+            width: 40px;
+            height: 40px;
+            border-radius: 50%;
+            margin-right: 10px;
+        }
+        .heading-logo {
+            margin-right: 10px
+        }
+        #admin-pages {
+            font-weight: normal;
+            opacity: 0.2;
+            padding-bottom: 0;
+        }
+        .pro-features {
+            opacity: 0.2;
+        }
+        .other-features {
+            opacity: 0.7;
+        }
+        #other-pages {
+            padding-bottom: 0;
+        }
+        .pro-badge {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            color:#603CFF;
+            font-weight: bold;
+            background-color:#F2EFFF;
+            border-radius: 39px;
+            width: 60px;
+            height: 30px;
+        }
+        .pro-feature-header {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+        }
+    </style>
+</head>
+<body>
+<div class="sidebar">
+    <div class="logo"><strong>CLOUDCHAT</strong>AI</div>
+    <nav class="nav flex-column">
+        <div class="nav-heading">
+            <img class="heading-logo" src="stars.png" alt="stars logo"> Chat UI
+        </div>
+        <div id="other-pages" class="nav-heading">
+            <img class="heading-logo" src="pagesIcon.png" alt="icon of pages">
+            Other Pages
+        </div>
+        <ul class="other-features">
+            Prompt Page
+        </ul>
+        <ul class="other-features">
+            Register
+        </ul>
+        <ul class="other-features">
+            Sign In
+        </ul>
+        <div class="pro-feature-header">
+            <div id="admin-pages" class="nav-heading">
+                <img class="heading-logo" src="lockedIcon.png" alt="locked icon ">
+                Admin Pages
+            </div>
+            <div class="pro-badge">
+                PRO
+            </div>
+        </div>
+            <ul class="pro-features">
+                All Templates
+            </ul>
+            <ul class="pro-features">
+                New Template
+            </ul>
+        <ul class="pro-features">
+            Edit Template
+        </ul>
+        <ul class="pro-features">
+            Users Overview
+        </ul>
+    </nav>
+    <div class="user-info">
+        <div style="display: flex; align-items: center">
+            <img src="Avatar.png" alt="User">
+            <div>Name Surname</div>
+        </div>
+        <div class="logout-button">
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)">
+                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_iconCarrier">
+                    <path d="M14 4L18 4C19.1046 4 20 4.89543 20 6V18C20 19.1046 19.1046 20 18 20H14M3 12L15 12M3
+                    12L7 8M3 12L7 16" stroke="#001A72" stroke-width="1.5" stroke-linecap="round"
+                          stroke-linejoin="round"></path>
+                </g>
+            </svg>
+        </div>
+    </div>
+</div>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
